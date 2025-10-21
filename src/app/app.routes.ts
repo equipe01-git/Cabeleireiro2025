476fe4f2fc2c5bc1f720ace3523dbc8da3738cbd
equipe01-git <projetomobile2502@gmail.com>
2025-10-21 14:53:08 -0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,16 +2,6 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'folder/inbox',
-    pathMatch: 'full',
-  },
-  {
-    path: 'folder/:id',
-    loadComponent: () =>
-      import('./folder/folder.page').then((m) => m.FolderPage),
-  },
-  {
     path: 'entrar',
     loadComponent: () => import('./entrar/entrar.page').then( m => m.EntrarPage)
   },
