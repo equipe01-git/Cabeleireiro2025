7b4c2f0945a29a74d0ca903d42395df233d62c70
equipe01-git <projetomobile2502@gmail.com>
2025-10-20 22:59:25 -0300

    Primeiro Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'CabeleireiroApp',
+  webDir: 'www'
+};
+
+export default config;
