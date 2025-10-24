fb59916bc4ce7143e0d5fd0760379adb3096de4d
MukaVPS <samuca.victorp14@gmail.com>
2025-10-24 16:03:26 -0300

    Update criar.page.ts

--- src/app/criar/criar.page.ts
+++ src/app/criar/criar.page.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonButton, IonInput, IonInputPasswordToggle } from '@ionic/angular/standalone';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-criar',
   templateUrl: './criar.page.html',
   styleUrls: ['./criar.page.scss'],
   standalone: true,
-  imports: [IonContent, CommonModule, FormsModule, IonButton, IonInput, IonInputPasswordToggle]
+  imports: [IonContent, CommonModule, FormsModule, IonButton, IonInput, IonInputPasswordToggle, RouterModule]
 })
 export class CriarPage implements OnInit {
 
