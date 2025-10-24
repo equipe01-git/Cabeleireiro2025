87965c45af45abbffdbbd38e2df08d319930413d
MukaVPS <samuca.victorp14@gmail.com>
2025-10-24 15:31:40 -0300

    Update entrar.page.ts

--- src/app/entrar/entrar.page.ts
+++ src/app/entrar/entrar.page.ts
@@ -2,14 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonButton, IonInput, IonInputPasswordToggle} from '@ionic/angular/standalone';
-
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-entrar',
   templateUrl: './entrar.page.html',
   styleUrls: ['./entrar.page.scss'],
   standalone: true,
-  imports: [IonContent, CommonModule, FormsModule, IonButton, IonInput, IonInputPasswordToggle]
+  imports: [IonContent, CommonModule, FormsModule, IonButton, IonInput, IonInputPasswordToggle, RouterModule]
 })
 export class EntrarPage implements OnInit {
 
