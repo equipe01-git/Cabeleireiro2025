59367daf03cb766a22f2ab15d231a99d14543a25
MukaVPS <samuca.victorp14@gmail.com>
2025-11-06 23:32:34 -0300

    Update profile.page.ts

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { IonButtons, IonTitle, IonBackButton ,IonHeader, IonContent, IonToolbar, IonItem, IonCard, IonCardTitle, IonCardHeader, IonCardContent, IonList , IonLabel, IonIcon } from '@ionic/angular/standalone';
 import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
+  imports: [IonButtons, IonTitle, IonBackButton ,IonHeader, IonContent, IonToolbar, IonItem, IonCard, IonCardTitle, IonCardHeader, IonCardContent, IonList , IonLabel, IonIcon]
 })
 export class ProfilePage implements OnInit {
   user: any;
