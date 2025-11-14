a49b534f2da7c6417c81509c98ef01fec4282d8f
LuizHMES <luizmautos9955@gmail.com>
2025-11-14 09:29:41 -0300

    Update criarservico.page.ts

--- src/app/criarservico/criarservico.page.ts
+++ src/app/criarservico/criarservico.page.ts
@@ -1,20 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonInput,
+  IonButton,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-criarservico',
   templateUrl: './criarservico.page.html',
   styleUrls: ['./criarservico.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    IonInput,
+    IonButton,
+  ],
 })
 export class CriarservicoPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
+
