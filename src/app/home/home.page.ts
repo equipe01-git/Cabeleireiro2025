84beaf85e35fd4a9ec45f8ca78a4ce146bede34a
MukaVPS <samuca.victorp14@gmail.com>
2025-11-14 08:33:16 -0300

    Update home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { IonTitle, IonHeader, IonContent, IonToolbar, IonIcon, IonButton } from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
-  imports: [IonTitle, IonHeader, IonContent, IonToolbar, IonIcon, IonButton]
+  imports: [IonTitle, IonHeader, IonContent, IonToolbar, IonIcon, IonButton, RouterLink]
 })
 export class HomePage {
   constructor(private navCtrl: NavController) {}
