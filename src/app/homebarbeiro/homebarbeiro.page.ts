8686bc458afd1bd15546b485b8df9eac298cc986
MukaVPS <samuca.victorp14@gmail.com>
2025-11-19 16:05:04 -0300

    Update homebarbeiro.page.ts

--- src/app/homebarbeiro/homebarbeiro.page.ts
+++ src/app/homebarbeiro/homebarbeiro.page.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { NavController } from '@ionic/angular';
-import { IonTitle, IonHeader, IonContent, IonToolbar, IonIcon, IonButton, IonItem, IonLabel, IonAccordion, IonAccordionGroup } from '@ionic/angular/standalone';
+import { IonTitle, IonHeader, IonContent, IonToolbar, IonIcon, IonItem, IonLabel, IonAccordion, IonAccordionGroup } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: './homebarbeiro.page.html',
   styleUrls: ['./homebarbeiro.page.scss'],
-  imports: [IonTitle, IonHeader, IonContent, IonToolbar, IonButton, IonIcon, IonItem, IonLabel, IonAccordion, IonAccordionGroup]
+  imports: [IonTitle, IonHeader, IonContent, IonToolbar, IonIcon, IonItem, IonLabel, IonAccordion, IonAccordionGroup]
 })
 export class HomebarbeiroPage {
   constructor() {}
