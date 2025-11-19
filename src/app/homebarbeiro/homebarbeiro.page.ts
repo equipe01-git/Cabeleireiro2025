f709535d788b697a5455677d2817eb31247ea34a
MukaVPS <samuca.victorp14@gmail.com>
2025-11-19 15:56:54 -0300

    Update homebarbeiro.page.ts

--- src/app/homebarbeiro/homebarbeiro.page.ts
+++ src/app/homebarbeiro/homebarbeiro.page.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { IonTitle, IonHeader, IonContent, IonToolbar, IonIcon, IonButton, IonItem, IonLabel, IonAccordion, IonAccordionGroup } from '@ionic/angular/standalone';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
